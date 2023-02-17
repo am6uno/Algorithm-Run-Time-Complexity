9a2a1e899588cbe28a656c6c131d7d700f148df8
Brandon <brandonleute@gmail.com>
2023-02-17 16:21:07 -0600

    homepage initial commit

--- ui/encounter/src/app/app.module.ts
+++ ui/encounter/src/app/app.module.ts
@@ -2,13 +2,29 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatIconModule}  from '@angular/material/icon'
+import { RouterModule } from '@angular/router';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomepageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    RouterModule.forRoot([
+      {path: '', component: HomepageComponent},
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
