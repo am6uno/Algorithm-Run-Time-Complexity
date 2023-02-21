938003e83092b0c492f17f641f0799f8e27100c1
Brandon <brandonleute@gmail.com>
2023-02-21 00:18:51 -0600

    add frontend testing

--- ui/encounter/src/app/homepage/homepage.component.spec.ts
+++ ui/encounter/src/app/homepage/homepage.component.spec.ts
@@ -1,21 +1,7 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { HomepageComponent } from './homepage.component';
+import { HomepageComponent } from "./homepage.component";
 
 describe('HomepageComponent', () => {
-  let component: HomepageComponent;
-  let fixture: ComponentFixture<HomepageComponent>;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ HomepageComponent ]
-    })
-    .compileComponents();
-
-    fixture = TestBed.createComponent(HomepageComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+  let component = HomepageComponent;
 
   it('should create', () => {
     expect(component).toBeTruthy();
