a37426ec17bac37c2d497e97857d0394d6cd99da
Brandon <brandonleute@gmail.com>
2023-03-20 13:56:55 -0500

    Start on tests for problem-creation.component.ts

--- ui/encounter/src/app/problem-creation/problem-creation.component.spec.ts
+++ ui/encounter/src/app/problem-creation/problem-creation.component.spec.ts
@@ -1,9 +1,52 @@
+import { waitForAsync, ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
+import { MockProblemService } from '../../mocks/problem.service.mock';
+import { ProblemService } from '../problem-service/problem.service';
 import { ProblemCreationComponent } from './problem-creation.component';
 
-describe('TeacherLoginComponent', () => {
+describe('ProblemCreationComponent', () => {
 
-  it('should create', () => {
-    expect(true).toBeTruthy();
-  });
+    describe('HeroComponent', () => {
+        let component: ProblemCreationComponent;
+        let fixture: ComponentFixture<ProblemCreationComponent>;
+        let problemService: ProblemService;
+        let router: Router;
+        let _snackBar: MatSnackBar;
+
+        beforeEach(waitForAsync(() => {
+            TestBed.configureTestingModule({
+                declarations: [ ProblemCreationComponent ],
+                providers: [
+                { provide: ProblemService, useValue: MockProblemService },
+                { provide: Router, Router},
+                { provide: MatSnackBar, MatSnackBar}
+                ]
+            }).compileComponents();
+        }));
+
+        beforeEach(() => {
+            fixture = TestBed.createComponent(ProblemCreationComponent);
+            problemService = TestBed.inject(ProblemService);
+            router = TestBed.inject(Router);
+            _snackBar = TestBed.inject(MatSnackBar);
+            component = fixture.componentInstance;
+            fixture.detectChanges();
+        });
+
+        it('should exist', () => {
+            expect(component).toBeDefined();
+        });
+
+        describe('getSourceCodeFromTextInput', () => {
+            it('should set teh values for sourceCode, complexity, hints when codeInput is longer than 0', ()=> {
+                component.codeInput = "int x = 5;\nint z = 6;";
+                component.getSourceCodeFromTextInput();
+                expect(component.sourceCode).toEqual(["int x = 5;", "int z = 6;"]);
+                expect(component.complexity.length).toEqual(2);
+                expect(component.hints.length).toEqual(2);
+            });
+        });
+
+    });
 });
-
---
+++ ui/encounter/src/mocks/problem.service.mock.ts
@@ -0,0 +1,25 @@
+import { jest } from '@jest/globals';
+
+export const mockProblem1 = {
+    id: 1,
+    name: "problem1",
+    sourceCode: ["int x = 4;", "int y = 5;"],
+    complexity: ["O(C)", "O(C)"],
+    totalScore: 2,
+    currentScore: 2,
+}
+export const mockProblem2 = {
+    id: 2,
+    name: "problem2",
+    sourceCode: ["int x = 4;", "int y = 5;"],
+    complexity: ["O(C)", "O(N)"],
+    totalScore: 2,
+    currentScore: 1,
+}
+
+const mockProblemList = jest.fn().mockReturnValue([mockProblem1, mockProblem2])
+
+export const MockProblemService = {
+    getAllProblems: mockProblemList,
+    addProblem: jest.fn()
+}
