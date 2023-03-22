6dcfde72f55fd7bddf0482a73ff05432d11c1417
Cole Gregory <cgregory902@gmail.com>
2023-03-22 18:25:36 -0500

    Embedded in Problem Class
    with '#' will be ignored, and an empty message aborts the commit.

--- src/main/java/com/complexity/encounter/problem/Problem.java
+++ src/main/java/com/complexity/encounter/problem/Problem.java
@@ -1,5 +1,6 @@
 package com.complexity.encounter.problem;
 
+import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
@@ -14,9 +15,12 @@ import lombok.NoArgsConstructor;
 public class Problem {
     @Id
     @GeneratedValue
+
     private long id;
     private String name;
+    @Embedded
     private String[] sourceCode; // stored as an array of lines
+    @Embedded
     private String[] complexity; // stored as an array of lines
     private int totalScore;     // this would need to be stored on the student account
     private int currentScore;   // this would need to be stored on the student account
