e075155d749316c3c4adf24e90d7720e71ac5d9d
Cole Gregory <cgregory902@gmail.com>
2023-03-22 18:25:12 -0500

    ProblemSet controller, etc. and Embedded changes

---
+++ src/main/java/com/complexity/encounter/problemset/ProblemSetRepository.java
@@ -0,0 +1,4 @@
+package com.complexity.encounter.problemset;
+import org.springframework.data.jpa.repository.JpaRepository;
+public interface ProblemSetRepository extends JpaRepository<ProblemSet, Long>{
+}
---
+++ src/main/java/com/complexity/encounter/problemset/ProblemSetService.java
@@ -0,0 +1,39 @@
+package com.complexity.encounter.problemset;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProblemSetService
+{
+    @Autowired
+    private ProblemSetRepository problemSetRepository;
+
+    public void saveProblemSet(ProblemSet set)
+    {
+        problemSetRepository.save(set);
+    }
+    public List<ProblemSet> getAllProblemSets()
+    {
+        return problemSetRepository.findAll();
+    }
+    public Optional<ProblemSet> getProblemSetById(long id)
+    {
+        return problemSetRepository.findById(id);
+    }
+    public void deleteProblemSet(long id)
+    {
+        problemSetRepository.deleteById(id);
+    }
+    public void updateProblemSet(ProblemSet set, Long id)
+    {
+        Optional<ProblemSet> updatedProblemSet = problemSetRepository.findById(id);
+        updatedProblemSet.get().setName(set.getName());
+        updatedProblemSet.get().setProblemList(set.getProblemList());
+        updatedProblemSet.get().setNumCompleted(set.getNumCompleted());
+        problemSetRepository.save(updatedProblemSet.get());
+    }
+}
