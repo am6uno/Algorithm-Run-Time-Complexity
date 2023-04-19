25591d9657e503ae99dec50bc47ae0fd806c81b5
citizencosimo <61855193+citizencosimo@users.noreply.github.com>
2023-04-19 15:54:36 -0500

    Student list updated.

--- ui/encounter/src/app/classroom-detail/classroom-detail.component.ts
+++ ui/encounter/src/app/classroom-detail/classroom-detail.component.ts
@@ -2,6 +2,7 @@ import {Component, Input, OnChanges, SimpleChanges} from '@angular/core';
 import {Classroom} from "../classroom";
 import {ClassroomService} from "../classroom-service/classroom.service";
 import {MatSnackBar} from "@angular/material/snack-bar";
+import {Student} from "../student";
 
 @Component({
   selector: 'app-classroom-detail',
@@ -11,15 +12,17 @@ import {MatSnackBar} from "@angular/material/snack-bar";
 export class ClassroomDetailComponent implements OnChanges{
   @Input() classroom: Classroom;
   message = '';
-  students = false;
+  students = true;
+  student_list: Student[]
+
 
   constructor(private classroomService:ClassroomService, private _snackBar: MatSnackBar ) {
   }
   ngOnChanges(changes: SimpleChanges) {
     console.log(changes);
-    const student_list = this.classroom.enrolled_students
-    console.log(student_list);
-    if (student_list.length === 0){
+    this.student_list = this.classroom.enrolled_students
+    console.log(this.student_list);
+    if (this.student_list.length === 0){
       this.students = false;
       this.message = 'No Students Currently Enrolled'
     }
